84370ffd798feada6180937790532d3f5c95f5f1
Thei S <theisoe2016@gmail.com>
2023-02-02 22:49:14 -0800

    calculator bonus feat

---
+++ JS101/lesson_2_new/calculator.js
@@ -0,0 +1,81 @@
+//Ask the user for the first number.
+//Ask the user for the second number.
+//Ask the user for an operator to perfom.
+//Perform the operation on the two numbers.
+//Print the result in the console.
+
+const MESSAGES = require('./calculator_messages.json');
+const readline = require("readline-sync");
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+while (true) {
+prompt(MESSAGES['welcome']);
+
+prompt("What is the first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmm...that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+prompt("What is the second number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt(MESSAGE['validName']);
+  number2 = readline.question();
+}
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("Must chose 1, 2, 3, 4");
+  operation = readline.question();
+}
+
+let output;
+switch (operation) {
+  case "1":
+    output = Number(number1) + Number(number2);
+    break;
+  case "2":
+    output = Number(number1) - Number(number2);
+    break;
+  case "3":
+    output = Number(number1) * Number(number2);
+    break;
+  case "4":
+    output = Number(number1) / Number(number2);
+    break;
+}
+
+while (invalidNumber(number1)) {
+  prompt("Hmm...that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+console.log(`The result is ${(output)}`);
+
+prompt("Would you like to make another calculation? ")
+let answer = readline.question();
+
+while (!["Yes", "y", "Y", "No", "n", "N"].includes(answer)) {
+  prompt("Invalid choice! Please choose 'y' or 'n'");
+  answer = readline.question();
+}
+
+if (answer[0] === "n" || answer[0] === "N") {
+  prompt("See you again!");
+  break;
+}
+
+};
+
